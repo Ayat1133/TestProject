46b537b035e79d545bd54cee8861bc078c5d43dd
hujei <hujei@10.0.0.24>
2024-02-02 16:39:20 +0200

    Calculate-simple-2

--- Test1/src/main/java/Test1/App.java
+++ Test1/src/main/java/Test1/App.java
@@ -1,17 +1,12 @@
 package Test1;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
-/**
- * Hello world!
- *
- */
+	public static String NumberToChar(int number) {
+		if(( 50 <=number)&(number<=59))
+			return "C";
+		else
+			return "error" ;
+	}
+	}
+
--- Test1/src/test/java/Test1/AppTest.java
+++ Test1/src/test/java/Test1/AppTest.java
@@ -13,8 +13,17 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void exe_2_partition_C_min_positive()
     {
-        assertTrue( true );
+    	String result = App.NumberToChar(50);
+    	String expected_result="C";
+        org.junit.Assert.assertEquals(expected_result , result , 0.00001);
+    }
+    @Test
+    public void exe_2_partition_C_min_plus1()
+    {
+    	String result = App.NumberToChar(51);
+    	String expected_result="C";
+        org.junit.Assert.assertEquals(expected_result , result , 0.00001);
     }
 }
